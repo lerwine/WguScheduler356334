992c61260bc796f841148a9a75fe32c3e57cc313
Leonard T. Erwine <lenny@erwinefamily.net>
2020-08-07 01:15:32 -0400

    Initial check-in

---
+++ app/src/main/java/Erwine/Leonard/T/wgusheduler356334/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package Erwine.Leonard.T.wgusheduler356334.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/Erwine/Leonard/T/wgusheduler356334/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package Erwine.Leonard.T.wgusheduler356334.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
